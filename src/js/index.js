4707025de22375ed4ef1bfe6edb3b3430023455b
yesusmigag <yesusmigag@gmail.com>
2019-09-21 00:49:11 +0000

    My first HEAD

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,27 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+
+  document.getElementById("escusa").innerHTML = temporal();
 };
+
+function temporal() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
